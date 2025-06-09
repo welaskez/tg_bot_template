f8a812c35d3c25ac49555d0513344af986d4737c
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-09 16:22:48 +0500

    feat: create repositories package with abstract repository class

---
+++ bot/repositories/abc.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    async def get(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def add(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def update(self, *args: Any, **kwargs: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def delete(self, *args: Any, **kwargs: Any) -> Any:
+        pass
