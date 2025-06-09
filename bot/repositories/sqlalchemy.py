8120cb2c76db936aa02d124809fe7c7266ac4c53
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-09 17:39:53 +0500

    feat: created sqlalchemy repository

---
+++ bot/repositories/sqlalchemy.py
@@ -0,0 +1,33 @@
+from typing import Any, Generic, Optional, cast
+
+from sqlalchemy import ColumnExpressionArgument, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeMeta
+from typing_extensions import TypeVar
+
+from .abc import AbstractRepository
+
+T = TypeVar("T", bound=DeclarativeMeta)
+
+
+class SQLAlchemyRepository(Generic[T], AbstractRepository):
+    model: type[T]
+
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def get(self, *conditions: ColumnExpressionArgument[Any]) -> Optional[T]:
+        return cast(Optional[T], await self._session.scalar(select(self.model).where(*conditions)))
+
+    async def add(self, **kwargs: Any) -> T:
+        entity = self.model(**kwargs)
+        self._session.add(entity)
+        return entity
+
+    async def update(self, entity: T, **kwargs: Any) -> T:
+        for k, v in kwargs.items():
+            setattr(entity, k, v)
+        return entity
+
+    async def delete(self, entity: T) -> None:
+        await self._session.delete(entity)
