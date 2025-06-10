7e9f84bbf1fd0d870faf174491c1e1575d30a670
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-10 17:33:31 +0500

    feat: created my chat member handler

--- bot/routers/my_chat_member/user.py
+++ bot/routers/my_chat_member/user.py
@@ -1,3 +1,20 @@
-from aiogram import Router
+import logging
+
+from aiogram import Router, types
+from aiogram.enums import ChatMemberStatus
+
+logger = logging.getLogger(name=__name__)
 
 router = Router(name=__name__)
+
+
+@router.my_chat_member()
+async def my_chat_member_updates(chat_member_updated: types.ChatMemberUpdated) -> None:
+    new_status = chat_member_updated.new_chat_member.status
+    old_status = chat_member_updated.old_chat_member.status
+
+    if new_status == ChatMemberStatus.KICKED:
+        logger.info(f"User {chat_member_updated.from_user.id} blocked bot")
+
+    if old_status == ChatMemberStatus.KICKED and new_status == ChatMemberStatus.MEMBER:
+        logger.info(f"User {chat_member_updated.from_user.id} unblocked bot")
