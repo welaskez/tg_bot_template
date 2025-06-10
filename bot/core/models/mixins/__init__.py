c99ea95f70b58dd386320d10f3b923d4b220fd8e
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-10 13:04:14 +0500

    feat: added int pk mixin

--- bot/core/models/mixins/__init__.py
+++ bot/core/models/mixins/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "CreatedAtMixin",
     "UuidPkMixin",
+    "IntPkMixin",
 )
 
 from .created_at_mixin import CreatedAtMixin
+from .int_pk_mixin import IntPkMixin
 from .uuid_pk_mixin import UuidPkMixin
