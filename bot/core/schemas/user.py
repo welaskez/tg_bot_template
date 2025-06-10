dec2f2873e07c9d71677618f2fcdb7df89f2a39a
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-10 16:41:48 +0500

    feat: create user service
    - created user dto

---
+++ bot/core/schemas/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    tg_id: int
+    username: str
+    name: str | None = None
+    info: str | None = None
+    photo: str | None = None
+
+
+class UserUpdate(BaseModel):
+    taps: int | None = None
+    username: str | None = None
+    name: str | None = None
+    info: str | None = None
+    photo: str | None = None
---
+++ bot/services/user.py
@@ -0,0 +1,41 @@
+from typing import Callable
+
+from core.models import User
+from core.schemas.user import UserCreate, UserUpdate
+from uow.abc import AbstractUOW
+
+from .abc import AbstractService
+
+
+class UserService(AbstractService):
+    def __init__(self, uow_factory: Callable[[], AbstractUOW]) -> None:
+        self._uow_factory = uow_factory
+
+    async def get_by_id(self, user_id: int) -> User | None:
+        async with self._uow_factory() as uow:
+            return await uow.users.get(User.id == user_id)
+
+    async def add(self, user_create: UserCreate) -> User:
+        async with self._uow_factory() as uow:
+            user = await uow.users.add(**user_create.model_dump())
+            await uow.commit()
+            return user
+
+    async def update(self, user_id: int, user_update: UserUpdate) -> User | None:
+        async with self._uow_factory() as uow:
+            user = await uow.users.get(User.id == user_id)
+
+            if user:
+                user = await uow.users.update(user, **user_update.model_dump(exclude_unset=True))
+                await uow.commit()
+
+                return user
+
+            return None
+
+    async def delete(self, user_id: int) -> None:
+        async with self._uow_factory() as uow:
+            user = await uow.users.get(User.id == user_id)
+            if user:
+                await uow.users.delete(user)
+                await uow.commit()
