6553941f743a25bad003b84e6380762b59922a7a
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-10 17:50:10 +0500

    feat: created is not registered filter

--- bot/filters/__init__.py
+++ bot/filters/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "IsAdmin",
     "IsRegistered",
+    "IsNotRegistered",
 )
 
 from .is_admin import IsAdmin
+from .is_not_registered import IsNotRegistered
 from .is_registered import IsRegistered
---
+++ bot/filters/is_not_registered.py
@@ -0,0 +1,13 @@
+from aiogram import types
+from aiogram.filters import Filter
+from dishka import FromDishka
+from services.user import UserService
+
+
+class IsNotRegistered(Filter):
+    async def __call__(self, message: types.Message, user_service: FromDishka[UserService]) -> bool:
+        if message.from_user:
+            user = await user_service.get_by_tg_id(tg_id=message.from_user.id)
+            return user is None
+
+        return False
