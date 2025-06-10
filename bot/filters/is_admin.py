3563a75fda4683d238c0fced5d1c4622ee6b3d70
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-10 22:31:51 +0500

    feat: load admin id from di

--- bot/filters/is_admin.py
+++ bot/filters/is_admin.py
@@ -1,10 +1,9 @@
 from aiogram import types
 from aiogram.filters import Filter
+from core.config import Settings
+from dishka import FromDishka
 
 
 class IsAdmin(Filter):
-    def __init__(self, admin_ids: list[int]) -> None:
-        self._admin_ids = admin_ids
-
-    async def __call__(self, message: types.Message) -> bool:
-        return message.from_user.id in self._admin_ids if message.from_user else False
+    async def __call__(self, message: types.Message, settings: FromDishka[Settings]) -> bool:
+        return message.from_user.id == settings.admin_id if message.from_user else False
