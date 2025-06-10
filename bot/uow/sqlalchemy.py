154a52d1dacc6d7aca7c23c5002539931d4d268f
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-10 16:41:17 +0500

    feat: setup users repo in __aenter__ method

--- bot/uow/sqlalchemy.py
+++ bot/uow/sqlalchemy.py
@@ -1,24 +1,30 @@
-from typing import Generic
+from types import TracebackType
+from typing import Optional, Self
 
-from repositories.sqlalchemy import SQLAlchemyRepository, T
+from repositories.user import UserRepository
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from .abc import AbstractUOW
 
 
-class SQLAlchemyUOW(Generic[T], AbstractUOW):
+class SQLAlchemyUOW(AbstractUOW):
     def __init__(self, session_pool: async_sessionmaker[AsyncSession]) -> None:
         self._session_pool = session_pool
         self._session: AsyncSession
-        self.repo: SQLAlchemyRepository[T]
 
-    async def __aenter__(self) -> "SQLAlchemyUOW[T]":
+    async def __aenter__(self) -> Self:
         self._session = self._session_pool()
-        self.repo = SQLAlchemyRepository(session=self._session)
+        self.users = UserRepository(session=self._session)
         return self
 
-    async def __aexit__(self) -> None:
-        await self.rollback()
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        if exc_type:
+            await self.rollback()
         await self._session.close()
 
     async def rollback(self) -> None:
