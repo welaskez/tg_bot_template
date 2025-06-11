d4d3aae4e5825ea508876a2ebb76e71c24df7f4b
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-11 15:52:26 +0500

    fix: added inject

--- bot/filters/is_admin.py
+++ bot/filters/is_admin.py
@@ -2,8 +2,10 @@ from aiogram import types
 from aiogram.filters import Filter
 from core.config import Settings
 from dishka import FromDishka
+from dishka.integrations.aiogram import inject
 
 
 class IsAdmin(Filter):
+    @inject
     async def __call__(self, message: types.Message, settings: FromDishka[Settings]) -> bool:
         return message.from_user.id == settings.admin_id if message.from_user else False
--- bot/filters/is_registered.py
+++ bot/filters/is_registered.py
@@ -1,10 +1,12 @@
 from aiogram import types
 from aiogram.filters import Filter
 from dishka import FromDishka
+from dishka.integrations.aiogram import inject
 from services.user import UserService
 
 
 class IsRegistered(Filter):
+    @inject
     async def __call__(self, message: types.Message, user_service: FromDishka[UserService]) -> bool:
         if message.from_user:
             user = await user_service.get_by_tg_id(tg_id=message.from_user.id)
