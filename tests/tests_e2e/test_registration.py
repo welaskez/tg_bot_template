812df5c1c988f92e54fe3527f7489e075bade4d5
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-12 17:01:56 +0500

    feat: added registration e2e tests

---
+++ tests/tests_e2e/test_registration.py
@@ -0,0 +1,50 @@
+import pytest
+from pydantic import ValidationError
+
+from core.schemas.user import UserCreate
+from services.user import UserService
+
+
+@pytest.mark.parametrize("register_passphrase,username", [("ASDF", "naz")])
+async def test_registration_with_correct_register_passphrase(
+    user_service: UserService,
+    register_passphrase: str,
+    username: str,
+) -> None:
+    user = await user_service.register(UserCreate(tg_id=1234, username=username), register_passphrase)
+
+    assert user.tg_id == 1234
+    assert user.username == username
+
+
+@pytest.mark.parametrize("register_passphrase,username", [("dfs", "naz")])
+async def test_registration_with_incorrect_register_passphrase(
+    user_service: UserService,
+    register_passphrase: str,
+    username: str,
+) -> None:
+    with pytest.raises(ValueError, match="Incorrect register passphrase!"):
+        await user_service.register(UserCreate(tg_id=1234, username=username), register_passphrase)
+
+
+@pytest.mark.parametrize("register_passphrase,username", [(None, "naz")])
+async def test_registration_without_register_passphrase(
+    user_service: UserService,
+    register_passphrase: str,
+    username: str,
+) -> None:
+    user = await user_service.register(UserCreate(tg_id=1234, username=username), register_passphrase)
+
+    assert user.tg_id == 1234
+    assert user.username == username
+
+
+@pytest.mark.parametrize("register_passphrase,username", [("ASDF", None)])
+async def test_registration_without_username(
+    user_service: UserService,
+    register_passphrase: str,
+    username: str,
+) -> None:
+    with pytest.raises(ValidationError):
+        await user_service.register(UserCreate(tg_id=1234, username=username), register_passphrase)
+
