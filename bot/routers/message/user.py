7f8a7f1670f1caf12951fc146203a39f34526955
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-12 14:34:45 +0500

    feat: implemented ping cmd

--- bot/routers/message/user.py
+++ bot/routers/message/user.py
@@ -1,3 +1,15 @@
-from aiogram import Router
+from aiogram import Router, types
+from aiogram.filters import Command
+from core.feature.registry import FeatureRegistry
+from dishka import FromDishka
+from dishka.integrations.aiogram import inject
 
 router = Router(name=__name__)
+
+
+@router.message(Command("ping"))
+@inject
+async def ping(message: types.Message, feature_registry: FromDishka[FeatureRegistry]) -> None:
+    feature = feature_registry.get_feature("ping")
+    if feature:
+        await message.answer(feature.text)
