68ff8fd4926f6e55cb0f54835c115092c1eb619a
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-12 14:35:06 +0500

    feat: created admin router
    - implemented creator text router

--- bot/routers/message/__init__.py
+++ bot/routers/message/__init__.py
@@ -2,7 +2,9 @@ __all__ = ("router",)
 
 from aiogram import Router
 
+from .admin import router as admin_router
 from .user import router as user_router
 
 router = Router(name=__name__)
 router.include_router(user_router)
+router.include_router(admin_router)
