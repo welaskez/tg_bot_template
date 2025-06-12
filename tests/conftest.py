e95178a39b52100ed6e5dd6e9e7286675e21f6bd
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-12 17:01:45 +0500

    fix: setup dishka container
    - added scopes
    - added settings fixture
    - added settings to user service fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
-from typing import Callable
+from typing import Any, AsyncGenerator, Callable
 
 import pytest
-
 from core.config import Settings
 from core.models import Base
 from core.models.engine import DatabaseHelper
+from core.providers.app import AppProvider
+from dishka import AsyncContainer, make_async_container
 from services.user import UserService
 from testcontainers.postgres import PostgresContainer
 from uow.abc import AbstractUOW
@@ -12,12 +13,21 @@ from uow.sqlalchemy import SQLAlchemyUOW
 
 
 @pytest.fixture()
+async def container() -> AsyncGenerator[AsyncContainer, Any]:
+    container = make_async_container(AppProvider())
+
+    yield container
+
+    await container.close()
+
+
+@pytest.fixture(scope="session")
 def postgres_container():
     with PostgresContainer(image="postgres:15", driver="asyncpg") as postgres:
         yield postgres
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def db_helper(postgres_container: PostgresContainer) -> DatabaseHelper:
     return DatabaseHelper(
         url=postgres_container.get_connection_url(driver="asyncpg"),
@@ -40,10 +50,15 @@ async def setup_db(db_helper: DatabaseHelper):
 
 
 @pytest.fixture()
+async def settings(container: AsyncContainer) -> Settings:
+    return await container.get(Settings)
+
+
+@pytest.fixture()
 async def sqla_uow_factory(db_helper: DatabaseHelper) -> Callable[[], AbstractUOW]:
     return lambda: SQLAlchemyUOW(session_pool=db_helper.session_pool)
 
 
 @pytest.fixture()
-def user_service(sqla_uow_factory: Callable[[], AbstractUOW]) -> UserService:
-    return UserService(sqla_uow_factory)
+def user_service(sqla_uow_factory: Callable[[], AbstractUOW], settings: Settings) -> UserService:
+    return UserService(sqla_uow_factory, settings)
