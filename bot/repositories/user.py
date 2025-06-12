9ba267efe26a554592346bc2d10ada67d4437239
welaskez <nazar.nikitenko.03@mail.ru>
2025-06-12 18:09:08 +0500

    feat: added get all taps & get top user methods to user repository

--- bot/repositories/user.py
+++ bot/repositories/user.py
@@ -1,5 +1,7 @@
 from core.models import User
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.functions import func
 
 from .sqlalchemy import SQLAlchemyRepository
 
@@ -9,3 +11,12 @@ class UserRepository(SQLAlchemyRepository[User]):
 
     def __init__(self, session: AsyncSession):
         super().__init__(session)
+
+    async def get_all_taps(self) -> int:
+        return await self._session.scalar(select(func.sum(User.taps))) or 0
+
+    async def get_top_user(self) -> User | None:
+        result: User | None = await self._session.scalar(
+            select(User).where(User.taps > 0).order_by(User.taps.desc()).limit(1)
+        )
+        return result
